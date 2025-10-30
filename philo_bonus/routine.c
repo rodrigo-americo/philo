6d711a673013ad1dceaee03486a123ec90436523
Rodrigo Gregorio <rgregori@c2r6p1.42sp.org.br>
2025-10-30 15:17:36 -0300

    coreção bonus de pontos de pensamenteos

--- philo_bonus/routine.c
+++ philo_bonus/routine.c
@@ -6,7 +6,7 @@
 /*   By: rgregori <rgregori@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 12:07:34 by rgregori          #+#    #+#             */
-/*   Updated: 2025/10/17 14:57:46 by rgregori         ###   ########.fr       */
+/*   Updated: 2025/10/30 15:10:34 by rgregori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,24 @@ static void	ft_release_forks(t_philo_state *state)
 	sem_post(state->seats);
 }
 
+void *philo_monitor(void *arg)
+{
+	t_philo_state *state;
+	
+	state = (t_philo_state *)arg;
+	while (1)
+	{
+		ft_check_death(state);
+		ft_check_finish(state);
+		usleep(1000);
+	}
+	return (NULL);
+}
+
 void	ft_philosopher_routine(t_philo_data data)
 {
 	t_philo_state	state;
+	pthread_t		monitor_tid;
 
 	ft_init_state(&state, data);
 	if (state.data.number_of_philosophers == 1)
@@ -40,13 +55,15 @@ void	ft_philosopher_routine(t_philo_data data)
 		ft_one_philo_eat(&state);
 		exit(0);
 	}
+	if (pthread_create(&monitor_tid, NULL, &philo_monitor, (void *)&state) != 0)
+		exit(1);
+	pthread_detach(monitor_tid);
 	while (1)
 	{
 		ft_philo_think(&state);
 		sem_wait(state.seats);
 		ft_philo_eat(&state);
 		ft_release_forks(&state);
-		ft_check_finish(&state);
 		ft_philo_sleep(&state);
 	}
 }
